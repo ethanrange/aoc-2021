65d0dc40d190c32a7e2ba3f1ab420ebfed70eaf9
Ethan Range <65268454+ethanrange@users.noreply.github.com>
2021-12-05 08:27:12 +0000

    Fix pattern match failure in flatmap

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -4,17 +4,17 @@ class Day5 {
   val lines = Source
     .fromFile("src/main/data/day5/input.txt")
     .getLines
-    .map(_.split(" -> |,") match {
-      case Array(sx, sy, ex, ey) =>
-        ((sx.toInt, sy.toInt), (ex.toInt, ey.toInt))
+    .map(_.split(" -> |,").map(_.toInt) match {
+      case Array(sx, sy, ex, ey) => ((sx, sy), (ex, ey))
     })
     .toSeq
 
   def computeOverlaps(list: Seq[((Int, Int), (Int, Int))], diag: Boolean): Int =
-    list.flatMap { case ((sx, sy), (ex, ey))
-        if (sx == ex || sy == ey || (diag && (sx - ex).abs == (sy - ey).abs)) =>
+    list.flatMap { case ((sx, sy), (ex, ey)) =>
+        if (sx == ex || sy == ey || (diag && (sx - ex).abs == (sy - ey).abs))
           (sx to ex by (if (sx < ex) 1 else -1))
             .zipAll(sy to ey by (if (sy < ey) 1 else -1), ex, ey)
+        else Seq.empty
       }
       .groupBy(identity)
       .count { case (x, l) => l.size > 1 }
